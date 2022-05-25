7676d5fbc2a35fe759c4b1e18e54b155a972a397
ElenaDierickx <elena.dierickx@outlook.be>
2022-05-25 14:01:50 +0200

    Added dependencies

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const { Client, Intents } = require('discord.js');
-require('dotenv').config();
-const token = process.env.DISCORD_TOKEN
+require("dotenv").config();
+const { Client, Intents } = require("discord.js");
+const token = process.env.DISCORD_TOKEN;
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.once('ready', () => {
-	console.log('Ready!');
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.login(token);
