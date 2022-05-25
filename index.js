40ed17e8937a2823af69f503f728b23f7bd67ae0
ElenaDierickx <elena.dierickx@outlook.be>
2022-05-25 14:19:50 +0200

    Added test commands

--- index.js
+++ index.js
@@ -8,4 +8,18 @@ client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === "ping") {
+        await interaction.reply("Pong!");
+    } else if (commandName === "server") {
+        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+    } else if (commandName === "user") {
+        await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+    }
+});
+
 client.login(token);
