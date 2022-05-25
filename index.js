8208191ced99af08e99bfb834de2f6e000d4a8c5
ElenaDierickx <elena.dierickx@outlook.be>
2022-05-25 14:41:39 +0200

    Command handling

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 require("dotenv").config();
-const { Client, Intents } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, Intents } = require("discord.js");
 const token = process.env.DISCORD_TOKEN;
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    client.commands.set(command.data.name, command);
+}
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
